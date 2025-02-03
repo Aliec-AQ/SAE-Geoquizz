6e80f44c5a002f50e53377437148ed39e70d72f1
EtiqueKevin <ketique04@gmail.com>
2025-02-03 12:08:08 +0100

    init Game

---
+++ api-geoquizz/config/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+
+return  [
+
+    'displayErrorDetails' => true,
+
+    'user.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file('iniconf/users.db.ini');
+        $dsn = "{$config['driver']}:host={$config['host']};port={$config['port']};dbname={$config['database']};";
+        $user = $config['username'];
+        $password = $config['password'];
+        return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+    },
+
+    'SECRET_KEY' => getenv('lJWT_SECRET_KEY'),
+
+];
